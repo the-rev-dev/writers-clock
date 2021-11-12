0889ab5f7ac23b15ff462963e9f395f778daa68b
Tim Ernst <timothyaernst@gmail.com>
2021-11-11 19:40:32 -0600

    Updated stuff

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from '../HomePage/HomePage';
-import { GamePage } from '../GamePage/GamePage';
 import { AppHeader } from '../AppHeader/AppHeader';
 // import { AppFooter } from '../AppFooter/AppFooter';
 
@@ -15,7 +14,6 @@ export const App: React.FC<{}> = () => {
       <Routes>
 
         <Route path="/" element={<HomePage />} />
-        <Route path="/game" element={<GamePage />}/>
 
       </Routes>
 
